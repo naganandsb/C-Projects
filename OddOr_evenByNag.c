44cd9173a1eea2bd238f8c0c2e31e64ab8af14ca
naganandsb <sb.nagananda@gmail.com>
2021-10-19 00:40:26 +0530

    Odd or Even By Nag
    Easy method to Odd or even

---
+++ OddOr_evenByNag.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+//	int a=12345;
+//	char Res[2][5] ={"Even","Odd"};
+//	printf("%s",Res[(a&1)]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a=-12345;
+//	char Res[2][5] ={"+ve","-ve"};
+//	printf("%d %s",a,Res[a<0]);
+//	return 0;
+//}
